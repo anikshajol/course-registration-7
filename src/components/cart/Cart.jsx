4e761f247881ad8cfedd13c4c68632cd944fe0ae
anikshajol <anikshajol@gmail.com>
2023-09-15 03:08:44 +0600

    cart components added

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,7 +1,17 @@
 const Cart = () => {
   return (
     <div>
-      <h1>I am ca</h1>
+      <h2 className="border-b-2 pb-4">Credit Hour Remaining 7 hr</h2>
+
+      <div className="py-4 border-b-2 pb-4">
+        <h2>Course Name</h2>
+      </div>
+      <div className="border-b-2 pb-4">
+        <h2 className="pt-4">Total Credit Hour : 13</h2>
+      </div>
+      <div className="pt-4">
+        <h2>Total Price : 48000 USD</h2>
+      </div>
     </div>
   );
 };
